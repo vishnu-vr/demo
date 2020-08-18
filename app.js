9fc721e912497e978e2927fb0277e0f4b25066fd
vishnuramesh <vishnuramesh19@gmail.com>
2020-08-19 01:39:22 +0530

    same

--- app.js
+++ app.js
@@ -32,6 +32,23 @@ app.get('/', function(req, res, next) {
 
 app.get('/update', function(req, res, next) {
   var ls = spawn("git", ["pull"]);
+  
+  ls.stdout.on("data", data => {
+      console.log(`stdout: ${data}`);
+  });
+
+  ls.stderr.on("data", data => {
+      console.log(`stderr: ${data}`);
+  });
+
+  ls.on('error', (error) => {
+      console.log(`error: ${error.message}`);
+  });
+
+  ls.on("close", code => {
+      console.log(`child process exited with code ${code}`);
+  });
+
   ls = spawn("pm2", ["restart", "app.js"]);
 
   ls.stdout.on("data", data => {
